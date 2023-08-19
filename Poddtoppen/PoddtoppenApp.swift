00d8ae7ac476301eaf6c3b2d060c426cb32881aa
Kristoffer Anger <kristoffer.anger@gmail.com>
2023-08-19 12:42:50 +0200

    Initial Commit

---
+++ Poddtoppen/PoddtoppenApp.swift
@@ -0,0 +1,17 @@
+//
+//  PoddtoppenApp.swift
+//  Poddtoppen
+//
+//  Created by Kristoffer Anger on 2023-08-19.
+//
+
+import SwiftUI
+
+@main
+struct PoddtoppenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
